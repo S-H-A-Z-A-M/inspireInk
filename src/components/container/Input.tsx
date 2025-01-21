5c1876bc371f81339064c0d46f7e9fa0d60fdf3b
tejasgambhir <tejasgambhir2003@gmail.com>
2025-01-21 14:45:02 +0530

    Develope footer component

--- src/components/container/Input.tsx
+++ src/components/container/Input.tsx
@@ -4,12 +4,16 @@ import React, { useId, forwardRef, InputHTMLAttributes } from "react";
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string; // Optional label text
   className?: string; // Optional custom class name
+
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
+const Input = React.forwardRef<HTMLInputElement, InputProps>
+(function Input(
   { label, type = "text", className = "", ...props },
   ref
 ) {
+  
+  console.log(props);
   const id = useId();
   return (
     <div className="w-full">
