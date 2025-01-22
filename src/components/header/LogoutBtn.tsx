fd2a53f62ed017b64618976128e5eb5474c1e30a
tejasgambhir <tejasgambhir2003@gmail.com>
2025-01-23 00:43:01 +0530

    Add profic pic dropdown

--- src/components/header/LogoutBtn.tsx
+++ src/components/header/LogoutBtn.tsx
@@ -18,12 +18,11 @@ function LogoutBtn() {
   };
 
   return (
-    <Button
+    <button
       onClick={handleLogout}
-      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
     >
       Logout
-    </Button>
+    </button>
   );
 }
 
