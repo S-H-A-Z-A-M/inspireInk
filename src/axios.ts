ac3218901ad4da3a3c3d6ea8bd5310e73415f27c
Gaurav Gangde <100701795+S-H-A-Z-A-M@users.noreply.github.com>
2025-02-05 19:07:57 +0530

    added backend url in axios

--- src/axios.ts
+++ src/axios.ts
@@ -3,19 +3,19 @@ import { store } from "./store/store";
 import { logout } from "./store/authSlice";
 
 const userApi = axios.create({
-  baseURL: "http://localhost:8080/api/v1/users",
+  baseURL: `${process.env.VITE_BACKEND_URL}/api/v1/users`,
   withCredentials: true, // this line made possible to have the cookie stored in the browser
   headers: {
     "Content-Type": "application/json",
   },
 });
 const blogApi = axios.create({
-  baseURL: "http://localhost:8080/api/v1/blogs", // Replace with your API base URL
+  baseURL: `${process.env.VITE_BACKEND_URL}/api/v1/blogs`, // Replace with your API base URL
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
 const commentApi = axios.create({
-  baseURL: "http://localhost:8080/api/v1/comments", // Replace with your API base URL
+  baseURL: `${process.env.VITE_BACKEND_URL}/api/v1/comments`, // Replace with your API base URL
   timeout: 5000,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
