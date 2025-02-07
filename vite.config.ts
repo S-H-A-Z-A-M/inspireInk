cc158513163665dc61c61bb889db0430e4cced4c
Gaurav Gangde <100701795+S-H-A-Z-A-M@users.noreply.github.com>
2025-02-07 17:58:00 +0530

    trying reverse proxy for the render cookie issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        // target: "https://inspireink-3lx5.onrender.com", // Change this to your backend URL
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
