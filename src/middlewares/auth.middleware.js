6234ddfbde5c0d03e920a9e898ae36f11f90f3a8
Gaurav Gangde <100701795+S-H-A-Z-A-M@users.noreply.github.com>
2025-02-08 14:53:58 +0530

    error solving in auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,8 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-
+    console.log(req.cookies)
+    console.log(token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
