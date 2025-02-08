e20552e721b1e154dd89262f23da59f841776c27
Gaurav Gangde <100701795+S-H-A-Z-A-M@users.noreply.github.com>
2025-02-08 14:16:01 +0530

    removing proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: "https://inspireink-3lx5.onrender.com", // Change this to your backend URL
-        target: "https://inspireink-3lx5.onrender.com",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       // target: "https://inspireink-3lx5.onrender.com", // Change this to your backend URL
+  //       target: "https://inspireink-3lx5.onrender.com",
+  //       changeOrigin: true,
+  //       secure: true,
+  //       rewrite: (path) => path.replace(/^\/api/, "/api"),
+  //     },
+  //   },
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
