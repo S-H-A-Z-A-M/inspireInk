8534d45c1b2a0977eae17a1b8c7ca6ec82eef8a0
Gaurav Gangde <100701795+S-H-A-Z-A-M@users.noreply.github.com>
2025-04-08 01:23:29 +0530

    made changes to firebase admin cause of fs.readfilesync

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -2,6 +2,13 @@ import admin from "firebase-admin";
 import fs from "fs";
 
 const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+if (!serviceAccountPath) {
+  throw new Error(
+    "Environment variable GOOGLE_APPLICATION_CREDENTIALS is not defined."
+  );
+}
+
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 if (!admin.apps.length) {
